0d12a71d2842dec356a61edaf2bd16632bab5ff5
AissistantPros <aissistant.auto@gmail.com>
2025-02-13 18:52:44 -0500

    prompt

--- consultarinfo.py
+++ consultarinfo.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 # Variables de configuración de Google Sheets
 GOOGLE_SHEETS_ID = config("GOOGLE_SHEETS_ID", default=None)
-GOOGLE_PRIVATE_KEY = config("GOOGLE_PRIVATE_KEY", default=None).replace("\\n", "\n")
+GOOGLE_PRIVATE_KEY = config("GOOGLE_PRIVATE_KEY", default=None)
 GOOGLE_PROJECT_ID = config("GOOGLE_PROJECT_ID", default=None)
 GOOGLE_CLIENT_EMAIL = config("GOOGLE_CLIENT_EMAIL", default=None)
 
