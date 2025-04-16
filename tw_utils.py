0c1c32046b7239b48c2908819280e9cfefaf1674
AissistantPros <aissistant.auto@gmail.com>
2025-04-16 18:31:04 -0500

    Pruebas  16Abr

--- tw_utils.py
+++ tw_utils.py
@@ -43,8 +43,8 @@ CALL_SILENCE_TIMEOUT = 30
 
 class TwilioWebSocketManager:
     def __init__(self):
-        self.accumulating_timeout_general = 1.5
-        self.accumulating_timeout_phone = 3.5
+        self.accumulating_timeout_general = 1.0
+        self.accumulating_timeout_phone = 2.5
         self.accumulating_timer_task = None
         self._reset_all_state()
 
