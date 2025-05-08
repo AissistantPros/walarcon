c33e4026ed8ff51a73eb40dec53c66e6951b44eb
AissistantPros <aissistant.auto@gmail.com>
2025-05-07 19:04:27 -0500

    Pruebas  7May

--- deepgram_stt_streamer.py
+++ deepgram_stt_streamer.py
@@ -52,7 +52,7 @@ class DeepgramSTTStreamer:
                 smart_format=True,
                 interim_results=True,
                 endpointing=False,
-                utterance_end_ms="1500",
+                utterance_end_ms="2000",
                 vad_events=True
             )
 
