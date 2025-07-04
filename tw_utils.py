82e2f111a5d853166fd7a1f265f8c7a38e27dc9e
AissistantPros <aissistant.auto@gmail.com>
2025-07-03 22:41:58 -0500

    Reduccion de prompt, ELabs WS

--- tw_utils.py
+++ tw_utils.py
@@ -49,7 +49,7 @@ LOG_TS_FORMAT = "%H:%M:%S.%f"
 # --- Constantes Configurables para Tiempos (en segundos) ---
 PAUSA_SIN_ACTIVIDAD_TIMEOUT = .35
 MAX_TIMEOUT_SIN_ACTIVIDAD = 5.0
-LATENCY_THRESHOLD_FOR_HOLD_MESSAGE = 10 # Umbral para mensaje de espera
+LATENCY_THRESHOLD_FOR_HOLD_MESSAGE = 100 # Umbral para mensaje de espera
 HOLD_MESSAGE_FILE = "audio/espera_1.wav" # Asegúrate que esta sea la ruta correcta a tu archivo mu-law
           
 
